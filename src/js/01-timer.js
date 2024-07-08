91b7557f75c579c0374ec3b7e07baa4c5c7aa283
MagdalenaRa <magdalenaarams@gmail.com>
2024-07-08 19:38:08 +0200

    update timer

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -1,26 +1,26 @@
-// Описаний в документації
+
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
+
 import "flatpickr/dist/flatpickr.min.css";
-// Описаний у документації
+
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
+
 import "izitoast/dist/css/iziToast.min.css"
 
 let userSelectedDate;
 let timeInterval;
 
-// Timer //
+
 const daysSpan = document.querySelector('span[data-days]');
 const hoursSpan = document.querySelector('span[data-hours]');
 const minutesSpan = document.querySelector('span[data-minutes]');
 const secondsSpan = document.querySelector('span[data-seconds]');
 
-// Button //
+
 const button = document.querySelector('button[data-start]');
 button.disabled = true;
 
-// Options //
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -44,19 +44,19 @@ const options = {
 flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
+  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
+  
   const days = Math.floor(ms / day);
-  // Remaining hours
+  
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
+  
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
